c8b8a1cd58ccf20f1298d163171ae3e6d29faea3
jchris1901 <jchris1901@gmail.com>
2016-03-26 17:44:49 +0530

    2.2 modified

--- Chapter-2/2.2.py
+++ Chapter-2/2.2.py
@@ -1,10 +1,43 @@
-#Exercise 2.3
+#Exercise 2.2
 
-#1 - Volume of sphere with radius 5
-volume = (4 * 3.14 * 5 * 5 * 5)/3
-print volume
+width = 17
+height = 12.0
+delimiter = '.'
+
+#1
+
+a = width/2
+print a
+print type(a)
 
 print "\n"
 
-#2 - Cost of 60 copies
+#2
 
+a = width/2.0
+print a
+print type(a)
+
+print "\n"
+
+#3
+
+a = height/3
+print a
+print type(a)
+
+print "\n"
+
+#4
+
+a = 1 + 2 * 5
+print a
+print type(a)
+
+print "\n"
+
+#5
+
+a = delimiter * 5
+print a
+print type(a)
