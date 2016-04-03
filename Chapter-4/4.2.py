cba08e5ce6fda9a9eb41b189f0c9eb398c3c654c
jchris1901 <jchris1901@gmail.com>
2016-04-03 12:57:34 +0530

    Updated 4.2

---
+++ Chapter-4/4.2.py
@@ -0,0 +1,60 @@
+from swampy.TurtleWorld import *
+from polygon import *
+""" For this program to work you need the previous exercise's solution too
+    Save the solution as polygon.py and execute this
+"""
+def petal(t, r, angle):
+    """Draws a petal using two arcs.
+
+    t: Turtle
+    r: radius of the arcs
+    angle: angle (degrees) that subtends the arcs
+    """
+    for i in range(2):
+        arc(t, r, angle)
+        lt(t, 180-angle)
+
+
+def flower(t, n, r, angle):
+    """Draws a flower with n petals.
+
+    t: Turtle
+    n: number of petals
+    r: radius of the arcs
+    angle: angle (degrees) that subtends the arcs
+    """
+    for i in range(n):
+        petal(t, r, angle)
+        lt(t, 360.0/n)
+
+
+def move(t, length):
+    """Move Turtle (t) forward (length) units without leaving a trail.
+    Leaves the pen down.
+    """
+    pu(t)
+    fd(t, length)
+    pd(t)
+
+
+world = TurtleWorld()
+bob = Turtle()
+bob.delay = 0.01
+
+# draw a sequence of three flowers, as shown in the book.
+move(bob, -100)
+flower(bob, 7, 60.0, 60.0)
+
+move(bob, 100)
+flower(bob, 10, 40.0, 80.0)
+
+move(bob, 100)
+flower(bob, 20, 140.0, 20.0)
+
+die(bob)
+
+# dump the contents of the campus to the file canvas.eps
+world.canvas.dump()
+
+wait_for_user()
+
